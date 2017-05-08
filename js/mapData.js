2d52eba9127249d1320ebacdd464fa06785f37a6
viniciusfolha <vof@cin.ufpe.br>
2017-05-08 05:55:42 -0300

    Lines in the map(unfinished), parser(unfinished, only convert the data_RIO), legend (imperfect)

--- js/mapData.js
+++ js/mapData.js
@@ -16,10 +16,14 @@ var g = svg.append("g");
 	/* We simply pick up the SVG from the map object */
 
 //////////////////
+// RIO_BRT to new format
+//var test = new Parser_RIO_BRT('data/RIO_BRT_GPS_T.json');
+//test.data();
 
 
 
-d3.json("data/RIO_BRT_GPS.json", function(error, data) {
+
+d3.json("data/RIO_BRT_GPS_T.json", function(error, data) {
 	if (error) return console.warn(error);
 
 
@@ -31,7 +35,7 @@ d3.json("data/RIO_BRT_GPS.json", function(error, data) {
 	    return this._div;
 	};
 
-	// method that we will use to update the control based on feature properties passed
+
 	info.update = function (veiculo) {
 	    this._div.innerHTML = '<h4>BRT</h4>' +  (veiculo ?
 	        '<b> Linha: ' + veiculo.linha + '</b><br /> Velocidade:' + veiculo.velocidade + ' km / h'
@@ -40,7 +44,51 @@ d3.json("data/RIO_BRT_GPS.json", function(error, data) {
 
 	info.addTo(map);
 
+
+
 	var color  = d3.scaleOrdinal(d3.schemeCategory20b);
+
+	// LENGENDA
+
+	var linhas_L = d3.map(data.veiculos, function(d){return d.linha;}).keys()
+
+	var legend = L.control({position: 'bottomright'});
+
+	legend.onAdd = function (map) {
+
+	    var div = L.DomUtil.create('div', 'info legend'),
+	        grades = [],
+	        labels = [];
+
+
+	        div.innerHTML = '<h4>Linhas:</h4> '
+	    for (var i = 0; i < linhas_L.length; i++) {
+
+	        div.innerHTML +=
+	            '<i style="background:' + color(linhas_L[i]) + '"></i> ' +
+	            linhas_L[i] + (linhas_L[i+1] ? '<br>' : '');
+	    }
+
+	    return div;
+	};
+
+	legend.addTo(map);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	data.veiculos.forEach(function(d) {
 			
 			d.LatLng = new L.LatLng(d.latitude,
@@ -63,6 +111,32 @@ d3.json("data/RIO_BRT_GPS.json", function(error, data) {
 				//
 			})
 			.attr("pointer-events","visible");  
+	
+	var toLine = d3.line()
+			.curve(d3.curveLinear)
+			.x(function(d){
+				return applyLatLngToLayer(d).x
+			})
+			.y(function(d){
+				return applyLatLngToLayer(d).y
+			});
+	function applyLatLngToLayer(d) {
+	    var y = d.latitude
+	    var x = d.longitude
+	    return map.latLngToLayerPoint(new L.LatLng(y, x))
+	}
+    var featureElement = g.selectAll("path")
+            .data(data.veiculos)
+            .enter()
+            .append("path")
+            .attr("class", "line")
+            .attr('d', function(d){return toLine( data.veiculos.filter(x => x.linha === d.linha  ));}   )
+			.attr("fill", "none")
+	      	.attr("stroke", "red")
+	      	.attr("stroke-linejoin", "round")
+	      	.attr("stroke-linecap", "round")
+	      	.attr("stroke-width", 1.5);
+	
 
 	map.on("viewreset", update);
 	map.on("moveend", update);
